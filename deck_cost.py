aafe90c62b11bd5ca018b24783d173ae0d3031c9
oppecker <oppecker@gmail.com>
2018-03-04 13:58:55 -0800

    added argparse then refactored and addressed pylint

--- deck_cost.py
+++ deck_cost.py
@@ -2,33 +2,42 @@
 from collections import defaultdict
 import json
 import time
+import argparse
 
 import requests
 
-with open('deck_file.txt', 'r') as f:
-    lines = [line.rstrip() for line in f]
 
-deck = defaultdict(dict)
-for line in lines:
-    quantity, name, card_set = line.split(';')
-    name = '+'.join(name.split(' '))
-    deck[name].update({card_set: {'quantity': quantity}})
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--deck_file', default='sample_deck_file.txt')
+    parser.add_argument('--output_file', default='output.txt')
+    args = parser.parse_args()
 
-for name, card_sets in deck.iteritems():
-    for card_set in card_sets:
-        url = ''.join(['https://api.scryfall.com/cards/named?exact=',
-                       name, '&set=', card_set])
-        r = requests.get(url)
-        r.raise_for_status()
-        card = json.loads(r.text)
-        deck[name][card_set].update({'usd': card['usd']})
-        #don't overload scryfall.com
-        time.sleep(1)
+    with open(args.deck_file, 'r') as f:
+        lines = [line.rstrip() for line in f]
 
-total = 0
-for name, card_sets in deck.iteritems():
-    for card_set, info in card_sets.iteritems():
-        total += float(info['usd']) * int(info['quantity'])
+    deck = defaultdict(dict)
+    for line in lines:
+        quantity, name, card_set = line.split(';')
+        name = '+'.join(name.split(' '))
+        deck[name].update({card_set: {'quantity': quantity}})
 
-print json.dumps(deck, indent=4)
-print 'total dollars: ' + str(total)
+    for name, card_sets in deck.iteritems():
+        for card_set in card_sets:
+            url = ''.join(['https://api.scryfall.com/cards/named?exact=',
+                           name, '&set=', card_set])
+            r = requests.get(url)
+            r.raise_for_status()
+            card = json.loads(r.text)
+            deck[name][card_set].update({'usd': card['usd']})
+            #don't overload scryfall.com
+            time.sleep(1)
+
+    total = 0
+    for name, card_sets in deck.iteritems():
+        for card_set, info in card_sets.iteritems():
+            total += float(info['usd']) * int(info['quantity'])
+
+    with open(args.output_file, 'w') as f:
+        json.dump(deck, f, indent=4)
+        f.write('\ntotal dollars: ' + str(total))
